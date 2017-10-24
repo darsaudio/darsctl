843249ca03962d06fa8e1e7929cf329dcf619ff9
shaowei <shaowei.wang@qq.com>
2017-10-24 20:22:15 +0800

    ...

---
+++ utils.c
@@ -0,0 +1,24 @@
+#include "utils.h"
+
+int 
+draw_sliderbar(WINDOW *scr, sliderbar_conf_t *conf)
+{
+    if (!scr || !conf)
+        return -1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ utils.h
@@ -0,0 +1,34 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+#include <ncurses.h>
+
+
+typedef struct {
+    int y;
+    int x;
+    int w;
+    int h;
+    char *label;
+
+    float min;
+    float max;
+    float value;
+
+} sliderbar_conf_t;
+
+int draw_sliderbar(WINDOW *scr, sliderbar_conf_t *conf);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif      // __UTILS_H__
